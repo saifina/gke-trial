8e322db5f07c8c5b205660e9a7e4eb12c8af62a8
Maredia, Saifina <Saifina.Maredia@ncr.com>
2020-06-17 21:16:59 -0400

    adding the source module file

---
+++ cluster.tf
@@ -0,0 +1,35 @@
+resource "google_container_cluster" "primary" {
+  name     = var.name
+  location = var.location
+  project  = var.project_id
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name     = "{var.name}-node-pool"
+  location = var.location
+  project  = var.project_id
+
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
